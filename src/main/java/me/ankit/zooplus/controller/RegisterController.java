6586e45c99f33e81ae6fd3632cc8ff9f044f9095
Bhurat <bhurata@michigan.gov>
2018-05-11 09:47:16 -0400

    Changed register.html, added name tag to the text boxes for mapping into Controller

--- src/main/java/me/ankit/zooplus/controller/RegisterController.java
+++ src/main/java/me/ankit/zooplus/controller/RegisterController.java
@@ -1,21 +1,39 @@
 package me.ankit.zooplus.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import me.ankit.zooplus.ILogging;
 import me.ankit.zooplus.Iconstants;
+import me.ankit.zooplus.Entity.Users;
+import me.ankit.zooplus.repositories.UserRepository;
 
 @Controller
-public class RegisterController {
+public class RegisterController implements ILogging {
 
-	@GetMapping("/register")
-	public String newUser() {
+	@Autowired
+	UserRepository userRepository;
+
+	@PostMapping("/register")
+	public String registeredUser(Users user) {
+
+		Log.info("RegisterController:registeredUser");
+
+		Log.info(user);		
+		user.setId(12L);
+
+		Log.info(user);
+
+		userRepository.save(user);
 		return Iconstants.PAGES.REGISTER.toString().toLowerCase();
 	}
 
-	@PostMapping("/register")
-	public String registeredUser() {
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public String newUser() {
+		Log.info("RegisterController:newUser");
 		return Iconstants.PAGES.REGISTER.toString().toLowerCase();
 	}
 
